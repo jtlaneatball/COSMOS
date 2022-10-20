b71043230e6f3e87c31dc96155cf51e70b5acd16
Lane, Josh <josh.lane@ballaerospace.com>
2022-10-19 18:08:30 -0600

    update build script to pull and tag base images for arm64

--- scripts/linux/cosmos_build_arm64.sh
+++ scripts/linux/cosmos_build_arm64.sh
@@ -13,28 +13,34 @@ export $(cat .env | grep -v "#" | xargs)
 # Buildx for multi-arch does not check the locally installed images.
 # It must pull base images from a registry, so push our base images to a local registry.
 
-docker pull minio/minio:RELEASE.2021-06-17T00-10-46Z@sha256:2bf13dd19c522585bca418abfac3783b328d723740758fdbe72fde61c55e4fdc
+docker pull --platform linux/arm64 alpine:${ALPINE_VERSION}.${ALPINE_BUILD}
+docker tag alpine:${ALPINE_VERSION}.${ALPINE_BUILD} ${COSMOS_REGISTRY}/alpine:${ALPINE_VERSION}.${ALPINE_BUILD}
+docker push ${COSMOS_REGISTRY}/alpine:${ALPINE_VERSION}.${ALPINE_BUILD}
+
+docker pull --platform linux/arm64 minio/minio:RELEASE.2021-06-17T00-10-46Z
 docker tag minio/minio:RELEASE.2021-06-17T00-10-46Z ${COSMOS_REGISTRY}/minio/minio:RELEASE.2021-06-17T00-10-46Z
 docker push ${COSMOS_REGISTRY}/minio/minio:RELEASE.2021-06-17T00-10-46Z
 
-docker pull minio/mc:RELEASE.2021-12-10T00-14-28Z@sha256:410ae817a76fbcc66a5fa06284f4823b9d834978aa8b7ff382862c28bffaf19a
+docker pull --platform linux/arm64 minio/mc:RELEASE.2021-12-10T00-14-28Z
 docker tag minio/mc:RELEASE.2021-12-10T00-14-28Z ${COSMOS_REGISTRY}/minio/mc:RELEASE.2021-12-10T00-14-28Z
 docker push ${COSMOS_REGISTRY}/minio/mc:RELEASE.2021-12-10T00-14-28Z
 
+docker pull --platform linux/arm64 redis:6.2
+docker tag redis:6.2 ${COSMOS_REGISTRY}/redis:6.2
+docker push ${COSMOS_REGISTRY}/redis:6.2
+
+docker pull --platform linux/arm64 traefik:2.7.0
+docker tag traefik:2.7.0 ${COSMOS_REGISTRY}/traefik:2.7.0
+docker push ${COSMOS_REGISTRY}/traefik:2.7.0
+
 docker buildx bake --set *.platform=linux/arm64 --load --no-cache -f compose.yaml -f compose-build.yaml --progress=plain cosmos-ruby
 docker push ${COSMOS_REGISTRY}/ballaerospace/cosmosc2-ruby:${COSMOS_TAG}
-# docker tag ballaerospace/cosmosc2-ruby:${COSMOS_TAG} localhost:5151/cosmosc2-ruby:${COSMOS_TAG}
-# docker push localhost:5151/cosmosc2-ruby:${COSMOS_TAG}
 
 docker buildx bake --set *.platform=linux/arm64 --load --no-cache -f compose.yaml -f compose-build.yaml --progress=plain cosmos-base
 docker push ${COSMOS_REGISTRY}/ballaerospace/cosmosc2-base:${COSMOS_TAG}
-# docker tag ballaerospace/cosmosc2-base:${COSMOS_TAG} localhost:5151/cosmosc2-base:${COSMOS_TAG}
-# docker push localhost:5151/cosmosc2-base:${COSMOS_TAG}
 
 docker buildx bake --set *.platform=linux/arm64 --load --no-cache -f compose.yaml -f compose-build.yaml --progress=plain cosmos-node
 docker push ${COSMOS_REGISTRY}/ballaerospace/cosmosc2-node:${COSMOS_TAG}
-# docker tag ballaerospace/cosmosc2-node:${COSMOS_TAG} localhost:5151/cosmosc2-node:${COSMOS_TAG}
-# docker push localhost:5151/cosmosc2-node:${COSMOS_TAG}
 
 docker buildx bake --set *.platform=linux/arm64 --load --no-cache -f compose.yaml -f compose-build.yaml --progress=plain \
     cosmos-minio \
