c6d560fa2eaf79e39337e91eabf2baf823035544
Lane, Josh <josh.lane@ballaerospace.com>
2023-05-10 17:32:52 -0600

    made amd and arm buildable with buildx, this branch name is a bit of a misnomer now

--- scripts/linux/cosmos_build_arm64.sh
+++ scripts/linux/cosmos_build_arm64.sh
@@ -13,36 +13,36 @@ export $(cat .env | grep -v "#" | xargs)
 # Buildx for multi-arch does not check the locally installed images.
 # It must pull base images from a registry, so push our base images to a local registry.
 
-docker pull --platform linux/arm64 alpine:${ALPINE_VERSION}.${ALPINE_BUILD}
+docker pull --platform ${PLATFORM} alpine:${ALPINE_VERSION}.${ALPINE_BUILD}
 docker tag alpine:${ALPINE_VERSION}.${ALPINE_BUILD} ${COSMOS_REGISTRY}/alpine:${ALPINE_VERSION}.${ALPINE_BUILD}
 docker push ${COSMOS_REGISTRY}/alpine:${ALPINE_VERSION}.${ALPINE_BUILD}
 
-docker pull --platform linux/arm64 minio/minio:RELEASE.2021-06-17T00-10-46Z
+docker pull --platform ${PLATFORM} minio/minio:RELEASE.2021-06-17T00-10-46Z
 docker tag minio/minio:RELEASE.2021-06-17T00-10-46Z ${COSMOS_REGISTRY}/minio/minio:RELEASE.2021-06-17T00-10-46Z
 docker push ${COSMOS_REGISTRY}/minio/minio:RELEASE.2021-06-17T00-10-46Z
 
-docker pull --platform linux/arm64 minio/mc:RELEASE.2021-12-10T00-14-28Z
+docker pull --platform ${PLATFORM} minio/mc:RELEASE.2021-12-10T00-14-28Z
 docker tag minio/mc:RELEASE.2021-12-10T00-14-28Z ${COSMOS_REGISTRY}/minio/mc:RELEASE.2021-12-10T00-14-28Z
 docker push ${COSMOS_REGISTRY}/minio/mc:RELEASE.2021-12-10T00-14-28Z
 
-docker pull --platform linux/arm64 redis:6.2
+docker pull --platform ${PLATFORM} redis:6.2
 docker tag redis:6.2 ${COSMOS_REGISTRY}/redis:6.2
 docker push ${COSMOS_REGISTRY}/redis:6.2
 
-docker pull --platform linux/arm64 traefik:2.7.0
+docker pull --platform ${PLATFORM} traefik:2.7.0
 docker tag traefik:2.7.0 ${COSMOS_REGISTRY}/traefik:2.7.0
 docker push ${COSMOS_REGISTRY}/traefik:2.7.0
 
-docker buildx bake --set *.platform=linux/arm64 --load --no-cache -f compose.yaml -f compose-build.yaml --progress=plain cosmos-ruby
+docker buildx bake --set *.platform=${PLATFORM} --load --no-cache -f compose.yaml -f compose-build.yaml --progress=plain cosmos-ruby
 docker push ${COSMOS_REGISTRY}/ballaerospace/cosmosc2-ruby:${COSMOS_TAG}
 
-docker buildx bake --set *.platform=linux/arm64 --load --no-cache -f compose.yaml -f compose-build.yaml --progress=plain cosmos-base
+docker buildx bake --set *.platform=${PLATFORM} --load --no-cache -f compose.yaml -f compose-build.yaml --progress=plain cosmos-base
 docker push ${COSMOS_REGISTRY}/ballaerospace/cosmosc2-base:${COSMOS_TAG}
 
-docker buildx bake --set *.platform=linux/arm64 --load --no-cache -f compose.yaml -f compose-build.yaml --progress=plain cosmos-node
+docker buildx bake --set *.platform=${PLATFORM} --load --no-cache -f compose.yaml -f compose-build.yaml --progress=plain cosmos-node
 docker push ${COSMOS_REGISTRY}/ballaerospace/cosmosc2-node:${COSMOS_TAG}
 
-docker buildx bake --set *.platform=linux/arm64 --load --no-cache -f compose.yaml -f compose-build.yaml --progress=plain \
+docker buildx bake --set *.platform=${PLATFORM} --load --no-cache -f compose.yaml -f compose-build.yaml --progress=plain \
     cosmos-minio \
     cosmos-redis \
     cosmos-cmd-tlm-api \
--- scripts/linux/cosmos_setup_arm64.sh
+++ scripts/linux/cosmos_setup_arm64.sh
@@ -37,18 +37,18 @@ else
 fi
 
 # Check if the cosmos5 builder already exists and create it if not
-if [ ! "$(docker buildx ls | grep 'cosmos5-armbuilder0' 2> /dev/null)" ]; then
+if [ ! "$(docker buildx ls | grep 'cosmos5-builder0' 2> /dev/null)" ]; then
     echo "creating cosmos multi-arch builder"
-    docker buildx create --platform linux/arm64 --driver-opt network=host --name cosmos5-armbuilder &>/dev/null
+    docker buildx create --platform linux/arm64,linux/amd64,windows/amd64 --driver-opt network=host --name cosmos5-builder &>/dev/null
 else
     echo "cosmos multi-arch builder already exists."
 fi
 
 # switch to and run the cosmos builder
-docker buildx use cosmos5-armbuilder &>/dev/null
+docker buildx use cosmos5-builder &>/dev/null
 docker buildx inspect --bootstrap &>/dev/null
 
 # copy ssl certificate to builder container
-docker cp cosmos-ruby/cacert.pem buildx_buildkit_cosmos5-armbuilder0:/usr/local/share/ca-certificates/ca-certificates.crt
+docker cp cosmos-ruby/cacert.pem buildx_buildkit_cosmos5-builder0:/usr/local/share/ca-certificates/ca-certificates.crt
 # update ssl certificates in builder container
-docker exec -d buildx_buildkit_cosmos5-armbuilder0 update-ca-certificates
+docker exec -d buildx_buildkit_cosmos5-builder0 update-ca-certificates
